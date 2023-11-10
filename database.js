432f24d484f7c5c1edd4855911d238dfb349420f
imparadu <imparadu@gmail.com>
2023-11-10 12:03:35 +0200

    pontez

--- database.js
+++ database.js
@@ -7,15 +7,31 @@ const obj = [
   },
 ];
 
-
 function pontez() {
-    lastWord = selectLastWord();
-    let tempObj = { date: "azi", ore: "mmmssssssssm" };
-    
-    obj.find((x) => x.name === lastWord).dates.push(tempObj);
-    
-    console.log(obj);
-    localStorage.setItem("dataBase", JSON.stringify(obj));
+  stop();
+
+  lastWord = selectLastWord();
+  let tempObj = { date: formattedDate, ore: `${spanHours.innerText}:${spanMinutes.innerText}:${spanSeconds.innerText}` };
+
+  obj.find((x) => x.name === lastWord).dates.push(tempObj);
+
+  console.log(obj);
+  localStorage.setItem("dataBase", JSON.stringify(obj));
 }
 
 const dataBase = localStorage.getItem("dataBase");
+
+var currentDate = new Date();
+
+var year = currentDate.getFullYear();
+var month = currentDate.getMonth() + 1; // Note: Months are zero-based
+var day = currentDate.getDate();
+
+var formattedDate =
+  year +
+  "-" +
+  (month < 10 ? "0" : "") +
+  month +
+  "-" +
+  (day < 10 ? "0" : "") +
+  day;
