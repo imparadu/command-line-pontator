5774f8de91508d538280cdd323b1de9037bd80d6
imparadu <imparadu@gmail.com>
2023-11-10 13:32:33 +0200

    working database

--- database.js
+++ database.js
@@ -1,37 +1,45 @@
-const obj = [
-  { id: 1, name: "radu", dates: [{ date: "data_curenta", ore: "nr_de_ore" }] },
-  {
-    id: 2,
-    name: "george",
-    dates: [{ date: "data_curenta", ore: "nr_de_ore" }],
-  },
-];
+// const obj = [
+//   { id: 1, name: "radu", dates: [{ date: "data_curenta", ore: "nr_de_ore" }] },
+//   {
+//     id: 2,
+//     name: "george",
+//     dates: [{ date: "data_curenta", ore: "nr_de_ore" }],
+//   },
+// ];
+const obj = JSON.parse(localStorage.getItem("dataBase"))
 
 function pontez() {
   stop();
-
+  
   lastWord = selectLastWord();
-  let tempObj = { date: formattedDate, ore: `${spanHours.innerText}:${spanMinutes.innerText}:${spanSeconds.innerText}` };
-
+  let tempObj = { date: localDate, ore: `${spanHours.innerText}:${spanMinutes.innerText}:${spanSeconds.innerText}` };
+  
   obj.find((x) => x.name === lastWord).dates.push(tempObj);
-
-  console.log(obj);
+  
   localStorage.setItem("dataBase", JSON.stringify(obj));
 }
 
 const dataBase = localStorage.getItem("dataBase");
+// console.log(dataBase)
+function returnFormattedDate(){
+  var currentDate = new Date();
 
-var currentDate = new Date();
-
-var year = currentDate.getFullYear();
-var month = currentDate.getMonth() + 1; // Note: Months are zero-based
-var day = currentDate.getDate();
-
-var formattedDate =
-  year +
-  "-" +
-  (month < 10 ? "0" : "") +
-  month +
-  "-" +
-  (day < 10 ? "0" : "") +
-  day;
+  var year = currentDate.getFullYear();
+  var month = currentDate.getMonth() + 1; // Note: Months are zero-based
+  var day = currentDate.getDate();
+  
+  var formattedDate =
+    year +
+    "-" +
+    (month < 10 ? "0" : "") +
+    month +
+    "-" +
+    (day < 10 ? "0" : "") +
+    day;
+  return formattedDate  
+}
+let localDate = returnFormattedDate()
+console.log(obj)
+obj.forEach(element => {
+  element.dates
+});
